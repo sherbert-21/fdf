68acf27dac6a72d6d83209cf94cc9df847926cc3
Silk Herbert <sherbert@oa-o1.msk.21-school.ru>
2021-11-12 18:17:47 +0300

    WOW LINES

--- src/screen.c
+++ src/screen.c
@@ -6,7 +6,7 @@
 /*   By: sherbert <sherbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 10:20:24 by sherbert          #+#    #+#             */
-/*   Updated: 2021/11/12 17:53:50 by sherbert         ###   ########.fr       */
+/*   Updated: 2021/11/12 18:16:09 by sherbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,21 @@
 
 void    bresenham(float x0, float y0, float x1, float y1, t_data *data)
 {
-    int error;
-    int deltaerr;
+    float max;
+    float x_step;
+    float y_step;
 
-    data->screen->x_step = abs(x1 - x0);
-    data->screen->y_step = abs(y1 - y0);
-    data->screen->y = y0;
-    data->screen->dir_y = y1 - y0;
-    if (data->screen->dir_y > 0)
-        data->screen->dir_y = 1;
-    if (data->screen->dir_y < 0)
-        data->screen->dir_y = -1;
-    data->screen->x = x0 - 1;
-    deltaerr = data->screen->y_step + 1;
-    error = 0;
-    while (++data->screen->x < x1)
+    max = fabsf(x1 - x0);
+    if (max < fabsf(y1 - y0))
+        max = fabsf(y1 - y0);
+    x_step = (x1 - x0) / max;
+    y_step = (y1 - y0) / max;
+    while ((int)(x1 - x0) || (int)(y1 - y0))
     {
-        mlx_pixel_put(data->mlx->mlx, data->mlx->win, data->screen->x, data->screen->y, 0xffffff);
-        error += deltaerr;
-        if (error >= (data->screen->x_step + 1))
-        {
-            data->screen->y += data->screen->dir_y;
-            error -= (data->screen->x_step + 1);
-        }
+        mlx_pixel_put(data->mlx->mlx, data->mlx->win, x0, y0, 0xffffff);
+        x0 += x_step;
+        y0 += y_step;
     }
-    // int max;
-
-    // data->screen->x_step = x1 - x0;
-    // data->screen->y_step = y1 - y0;
-    // max = data->screen->y_step;
-    // if (data->screen->x_step > data->screen->y_step)
-    //     max = data->screen->x_step;
-    // data->screen->x_step /= max;
-    // data->screen->y_step /= max;
-    // while ((int)(x0 - x1) || (int)(y0 - y1))
-    // {
-    //     mlx_pixel_put(data->mlx->mlx, data->mlx->win, x0, y1, 0xffffff);
-    //     x0 += data->screen->x_step;
-    //     y0 += data->screen->y_step;
-    // }
 }
 
 void    draw(t_data *data)
@@ -63,12 +39,11 @@ void    draw(t_data *data)
 
     y = -1;
     zoom = 30;
-    while (++y < data->height)
+    while (++y <= data->height)
     {
         x = -1;
-        while (++x < data->width)
+        while (++x <= data->width)
         {
-            ft_printf("(%d, %d)\t", x, y);
             bresenham(x * zoom, y * zoom, (x + 1) * zoom, y * zoom, data);
             bresenham(x * zoom, y * zoom, x * zoom, (y + 1) * zoom, data);
         }
